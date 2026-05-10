fc267b791d1f9fc204256275d09315197beb1665
ElizavetaMenshikova <mefmag01@gmail.com>
2026-05-10 17:40:54 +0300

    Добавлен CLI и заглушки

--- src/image_convolution/convolver.py
+++ src/image_convolution/convolver.py
@@ -1,2 +1,5 @@
-def placeholder() -> None:
-    pass
+import numpy as np
+from numpy.typing import NDArray
+
+def apply_convolution(image: NDArray[np.float32], kernel_name: str, border_type: str) -> NDArray[np.float32]:
+    return image
---
+++ src/image_convolution/main.py
@@ -0,0 +1,30 @@
+import argparse
+from .utils import load_image, save_image
+from .convolver import apply_convolution
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Свертка изображений")
+
+    parser.add_argument("input", help="Входной файл")
+    parser.add_argument("output", help="Выходной файл")
+
+    parser.add_argument("--kernel", default="blur", help="Выбор фильтра")
+    parser.add_argument("--border", default="constant", help="Тип обработки края")
+
+    args = parser.parse_args()
+    
+    image = load_image(args.input)
+
+    result = apply_convolution(
+        image=image,
+        kernel_name=args.kernel,
+        border_type=args.border
+    )
+
+    save_image(result, args.output)
+    print("Обработка завершена успешно!")
+
+
+if __name__ == "__main__":
+    main()
