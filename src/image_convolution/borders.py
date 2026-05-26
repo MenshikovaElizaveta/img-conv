c7667d7191599d02ee246740114c03cf37fb05e8
ElizavetaMenshikova <mefmag01@gmail.com>
2026-05-26 20:31:35 +0300

    Добавлены ядра свёртки и обработка краев

---
+++ src/image_convolution/borders.py
@@ -0,0 +1,65 @@
+from typing import cast
+
+import numpy as np
+from numpy.typing import NDArray
+
+
+def reflect_indices(
+    indices: NDArray[np.int_],
+    size: int,
+) -> NDArray[np.int_]:
+    period = 2 * size - 2
+
+    indices = np.abs(indices)
+    indices %= period
+
+    return np.where(
+        indices >= size,
+        period - indices,
+        indices,
+    )
+
+
+def apply_border(
+    image: NDArray[np.float32],
+    pad: int,
+    border_type: str,
+) -> NDArray[np.float32]:
+    h, w, c = image.shape
+    out_h, out_w = h + 2 * pad, w + 2 * pad
+
+    y_src = np.arange(out_h) - pad
+    x_src = np.arange(out_w) - pad
+
+    if border_type == "constant":
+        result = np.zeros((out_h, out_w, c), dtype=np.float32)
+        result[pad:pad + h, pad:pad + w] = image
+        return result
+
+    if border_type == "reflect":
+        y_src = reflect_indices(y_src, h)
+        x_src = reflect_indices(x_src, w)
+
+    elif border_type == "replicate":
+        y_src = np.clip(y_src, 0, h - 1)
+        x_src = np.clip(x_src, 0, w - 1)
+
+    elif border_type == "wrap":
+        y_src %= h
+        x_src %= w
+
+    else:
+        raise ValueError(
+            f"Такого типа обработки края нет: {border_type}"
+        )
+
+    y = y_src[:, np.newaxis]
+    x = x_src[np.newaxis, :]
+
+    return cast(
+        NDArray[np.float32],
+        np.asarray(
+            image[y, x],
+            dtype=np.float32,
+        ),
+    )
--- src/image_convolution/utils.py
+++ src/image_convolution/utils.py
@@ -1,9 +1,21 @@
 import numpy as np
 from numpy.typing import NDArray
+from PIL import Image
 
 
 def load_image(path: str) -> NDArray[np.float32]:
-    return np.array([[0.0]], dtype=np.float32)
+    image = Image.open(path).convert("RGB")
 
-def save_image(image: NDArray[np.float32], path: str) -> None:
-    pass
+    return np.array(image, dtype=np.float32)
+
+
+def save_image(
+    image: NDArray[np.float32],
+    path: str,
+) -> None:
+
+    clipped = np.clip(image, 0, 255).astype(np.uint8)
+
+    result = Image.fromarray(clipped)
+
+    result.save(path)
